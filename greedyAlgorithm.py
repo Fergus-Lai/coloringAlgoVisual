a8fd56991c6b9f4aa136a83fd62afdb226905402
Fergus Lai <ferguslai724@gmail.com>
2022-10-10 15:52:44 +0100

    Moved greedy algorithm into greedyAlgorithm.py

---
+++ greedyAlgorithm.py
@@ -0,0 +1,14 @@
+def greedyAlgorithm(colours, graph):
+    for node in graph:
+        if node.colour != "":
+            continue
+        usedColours = set()
+        for connectedNode in node.connected:
+            usedColours.add(connectedNode.colour)
+        for colour in colours:
+            if not colour in usedColours:
+                node.colour = colour
+                break
+        if node.colour == "":
+            return None
+    return graph
