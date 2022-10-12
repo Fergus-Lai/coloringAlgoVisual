97f7f3d41d86c5793937e9f01e278c4eef8b4ed4
Fergus Lai <ferguslai724@gmail.com>
2022-10-12 11:45:34 +0100

    Added Comments

--- greedyAlgorithm.py
+++ greedyAlgorithm.py
@@ -1,14 +1,17 @@
 def greedyAlgorithm(colours, graph):
-    for node in graph:
-        if node.colour != "":
+    for node in graph:  # Iterate Thorugh Nodes In Graph
+        if node.colour != "":  # Skipping Node Colored
             continue
-        usedColours = set()
+        usedColours = set()  # Storing color of nodes connected
+        # Iterating Thorugh Nodes Storing Color
         for connectedNode in node.connected:
             usedColours.add(connectedNode.colour)
+        # Iterate Thorugh List of Colours To Use and Choose First Colour Usable
         for colour in colours:
             if not colour in usedColours:
                 node.colour = colour
                 break
+        # No Colour Available
         if node.colour == "":
             return None
     return graph
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from constant import *
 
-pygame.init
 
-width, height = 900, 900
+# Init Pygame Window
+pygame.init
 screen = pygame.display.set_mode((width, height))
 
 running = True
 while running:
+    # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
